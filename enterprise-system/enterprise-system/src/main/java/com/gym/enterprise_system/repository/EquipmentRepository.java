dc352d12f3c0b652b3d89460efb02f653b43c4b4
thedivine-mishap <abdullahalfahad230@gmail.com>
2026-03-01 10:15:00 +0600

    Fix member profile continuous database querying bug

---
+++ enterprise-system/enterprise-system/src/main/java/com/gym/enterprise_system/repository/EquipmentRepository.java
@@ -0,0 +1,11 @@
+package com.gym.enterprise_system.repository;
+
+import com.gym.enterprise_system.entity.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, UUID> {
+}
