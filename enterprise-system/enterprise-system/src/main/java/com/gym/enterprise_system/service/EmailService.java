65dd6da8457b4f1f1c6ab59a20925ed9cc13b037
SazidRahman226 <rahmansazid2@gmail.com>
2026-03-05 23:15:00 +0600

    Final UI polish and unused code removal in Member Calendar

--- enterprise-system/enterprise-system/src/main/java/com/gym/enterprise_system/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.gym.enterprise_system.service;
-
-public interface EmailService {
-    void sendTrainerWelcomeEmail(String toEmail, String firstName);
-
-    void sendPasswordResetEmail(String toEmail, String token);
-
-    void sendStaffWelcomeEmail(String toEmail, String firstName, String tempPassword);
-}
